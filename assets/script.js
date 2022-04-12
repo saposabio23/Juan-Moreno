f439dead6a718c3131b8863d762f549cc615e2f7
Pablo Moreno <ehtomer@gmail.com>
2022-04-12 18:31:55 +0200

    text change

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,9 @@
 
 var $langUS = document.querySelector('#langUS');
 var $langES = document.querySelector('#langES');
-var $langFR = document.querySelector('#langFR');
 
 var $contentUS = document.querySelector('.contentUS');
 var $contentES = document.querySelector('.contentES');
-var $contentFR = document.querySelector('.contentFR');
 
 var $language = document.querySelector('.language');
 
@@ -20,11 +18,5 @@ function showES() {
   $language.classList.add("hide");
 }
 
-function showFR() {
-  $contentFR.classList.replace("hide", "show");
-  $language.classList.add("hide");
-}
-
 $langUS.addEventListener("click", showUS ,false);
 $langES.addEventListener("click", showES, false);
-$langFR.addEventListener("click", showFR, false);
